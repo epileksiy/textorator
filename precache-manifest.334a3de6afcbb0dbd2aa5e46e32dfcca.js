3fb9313b1a4f45169a4976073681fd45797e5805
Alex <richyshark@gmail.com>
2019-10-27 22:27:55 +0300

    Updates

---
+++ precache-manifest.334a3de6afcbb0dbd2aa5e46e32dfcca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "006c9d2efa171fdef9df0eb725c55de5",
+    "url": "/textorator/index.html"
+  },
+  {
+    "revision": "f8439bf013ad26a33049",
+    "url": "/textorator/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "6a49912fa8696d8c773c",
+    "url": "/textorator/static/css/main.e8d55ce2.chunk.css"
+  },
+  {
+    "revision": "f8439bf013ad26a33049",
+    "url": "/textorator/static/js/2.10f262cc.chunk.js"
+  },
+  {
+    "revision": "6a49912fa8696d8c773c",
+    "url": "/textorator/static/js/main.54943490.chunk.js"
+  },
+  {
+    "revision": "bf2b62ac1ff22bef8263",
+    "url": "/textorator/static/js/runtime-main.83e9ca60.js"
+  }
+]);
